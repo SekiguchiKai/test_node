14fb02acede1d3df31a35abe41cd61d8cd642c40
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-10-19 17:32:46 +0900

    ソースコードのレイアウトを改良

--- janken.js
+++ janken.js
@@ -1,19 +1,15 @@
-require 
-
-
-
 // 通常は、sに引数を与えない。テストの場合のみ与える
 exports.janken = function janken(c, s) {
 
     // クライアントの打ち手を代入    
     clientUchite = c;
 
-    
+
     // サーバ側の打ち手を決める
     // 乱数を発生させる
     serverUchite = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 
-// テスト用の確認
+    // テスト用の確認
     if (s != null) {
         serverUchite = s;
     }
