c2fef52a55f7682a26f006f6dff65a5db8e7779c
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-10-21 17:43:49 +0900

    janken.jsの連携完了-ただし、そのレスポンスは返せていない

---
+++ src/janken.js
@@ -0,0 +1,48 @@
+// じゃんけんのアルゴリズム部分（勝敗を決定してresultを返す）
+/*c = クライアントの打ち手、 s = サーバの打ち手（デフォルト値）
+cは、クライアントから送られてくる、sは、デフォルト値を使うこととし、テストの場合のみ変数を与える
+*/
+
+//クライアントとサーバのじゃんけんの結果を判定する関数
+exports.judgeResult = function judgeResult(c = 1, s = Math.floor(Math.random() * (2 - 0 + 1)) + 0) {
+
+    // クライアントの打ち手に引数を代入
+    var clientUchite = c;
+
+    // サーバの打ち手を代入に引数を代入
+    var serverUchite = s;
+
+    // じゃんけんの結果を格納する変数を宣言
+    var result;
+
+
+    // アルゴリズム
+    if ((clientUchite === 0 && serverUchite === 1) || (clientUchite === 1 && serverUchite === 2) || (clientUchite === 2 && serverUchite === 0)) {
+        result = "君の勝ちだ！";
+    }
+    else if ((clientUchite === 1 && serverUchite === 0) || (clientUchite === 2 && serverUchite === 1) || (clientUchite === 0 && serverUchite === 2)) {
+        result = "君の負けだ！";
+    }
+    else if (clientUchite === serverUchite) {
+        result = "引き分けだ！";
+    }
+
+    console.log(result);
+    return result;
+}
+
+
+// テストのための仕掛け
+// テスト時に任意のc = クライアントの打ち手、 s = サーバの打ち手を引数にすることができる
+exports.judgeResultTest = function judgeResultTest(c, s = Math.floor(Math.random() * (2 - 0 + 1)) + 0) {
+    var jk = require("./janken.js")
+
+    // 配列結果を格納する配列を作成
+    var resultArray;
+
+    // じゃんけんの結果をresultArray（配列）に代入
+    resultArray = (jk.judgeResult(c, s));
+
+    return resultArray;
+
+}
---
+++ src/webServer.js
@@ -0,0 +1,74 @@
+//クライアントからのレスポンスを受け取り、適切なファイルに処理を依頼する
+
+var http = require('http');
+var url = require('url');
+var server = http.createServer();
+var request = require('request');
+var qs = require('querystring');
+var path = require("path");
+
+//janken.jsを呼び出すためにrequire
+var jk = require("./janken.js");
+//responseGenerater.jsを呼び出すためにrequire
+var rG = require("./responseGenerater.js");
+
+
+// http.createServerがrequestされたら、
+exports.webserver = server.on('request', function (req, res) {
+
+    //ここで、下の関数を呼び出す（引数にここの関数のreq, resのを渡す）
+
+    // ①parseHttpMethodを呼び出す
+    parseHttpMethod(req, res);
+
+    // ②Handleを呼び出す
+    Handle(req, res);
+
+    // ①HTTPのクライアントからびリクエストのメソッドの解析を行う関数
+    function parseHttpMethod(req, res) {
+        // GETかPOSTか調べ、POSTの場合はそのボディを変数に格納する
+        if (req.method == 'GET') {
+            // URLを解析して、URLデーコディングする　（GETはURLでパラメータが来るので）
+            var urlParts = url.parse(req.url, true);
+            console.log(urlParts.query);
+        } else if (req.method == 'POST') {
+            //POSTリクエストのボディを格納する (POSTはリクエストのボディにパラメータが格納されるから)
+            //POSTリクエストのボディを格納するための変数を宣言する
+            var body = '';
+            //リクエストが来たら、そのパラメータをボディ(body)に格納する
+            req.on('data', function (data) {
+                body += data;
+            });
+            //リクエストが終了したら、コールバック関数（）を呼び出し
+            req.on('end', function () {
+                //body（クエリ）をパース
+                var POST = qs.parse(body);
+                console.log(POST);
+            });
+        }
+    }
+
+
+    // ②urlを解析し、ハンドラを呼び出す関数
+    function Handle(req, res) {
+        // urlのpathをuriに代入
+        var uri = url.parse(req.url).pathname;
+        // cwd()：カレントディレクトリ、uri：path
+        var filename = path.join(process.cwd(), uri);
+
+        // URIで行う処理を分岐させる
+        if (uri === "/") {
+            //responseGenerater.jsのhtmlGeneratorを呼び出す
+            //メモ：index.HTMLなど具体的なファイルはこちらから呼び出す
+            //responseGenerater.jsでは、一般的な操作だけを定義し、同じような処理を繰り返させない
+            rG.responseGenerator(res, '../template/index.html', 'text/html'); //res, resource, content_Type
+        } else if (uri === "/calcprocess") {
+            jk.judgeResult();
+        }
+    }
+
+});
+
+// 指定されたポート(8080)でコネクションの受け入れを開始する
+server.listen(8080)
+console.log('Server running at http://localhost:8080/');
