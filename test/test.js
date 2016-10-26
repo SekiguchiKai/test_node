d33e5ac5cc8ff9138d3022ac9e24f952eca3a382
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-10-26 11:01:49 +0900

    test.jsのobjConvertorのスペルをobjConverterに変更

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ const UCHITE_PA = 2;
 
 
 // judgeResultの戻り値のオブジェクトの中から、resultのみを取り出す関数
-function objConvetor(client, sever) {
+function objConverter(client, sever) {
     var objFolder;
     objFolder = jk.judgeResult(client, sever).result;
     return objFolder;
@@ -25,39 +25,39 @@ describe('じゃんけんアルゴリズム', function () {
     // 事前に行う処理
 
     it('「クライアント:グー、サーバ:チョキ」の場合は、クライアントの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_GU, UCHITE_TYOKI), "君の勝ちだ！");
+        assert.equal(objConverter(UCHITE_GU, UCHITE_TYOKI), "君の勝ちだ！");
     });
 
     it('クライアント:チョキ、サーバ:パー」の場合は、クライアントの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_TYOKI, UCHITE_PA), "君の勝ちだ！");
+        assert.equal(objConverter(UCHITE_TYOKI, UCHITE_PA), "君の勝ちだ！");
     });
 
     it('クライアント:パー、サーバ:グー」の場合は、クライアントの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_PA, UCHITE_GU), "君の勝ちだ！");
+        assert.equal(objConverter(UCHITE_PA, UCHITE_GU), "君の勝ちだ！");
     });
 
     it('クライアント:チョキ、サーバ:グー」の場合は、サーバの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_TYOKI, UCHITE_GU), "君の負けだ！");
+        assert.equal(objConverter(UCHITE_TYOKI, UCHITE_GU), "君の負けだ！");
     });
 
     it('クライアント:パー、サーバ:チョキ」の場合は、サーバの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_PA, UCHITE_TYOKI), "君の負けだ！");
+        assert.equal(objConverter(UCHITE_PA, UCHITE_TYOKI), "君の負けだ！");
     });
 
     it('クライアント:グー、サーバ:パー」の場合は、サーバの勝ち', function () {
-        assert.equal(objConvetor(UCHITE_GU, UCHITE_PA), "君の負けだ！");
+        assert.equal(objConverter(UCHITE_GU, UCHITE_PA), "君の負けだ！");
     });
 
     it('クライアント:グー、サーバ:グー」の場合は、引き分け', function () {
-        assert.equal(objConvetor(UCHITE_GU, UCHITE_GU), "引き分けだ！");
+        assert.equal(objConverter(UCHITE_GU, UCHITE_GU), "引き分けだ！");
     });
 
     it('クライアント:チョキ、サーバ:チョキ」の場合は、引き分け', function () {
-        assert.equal(objConvetor(UCHITE_TYOKI, UCHITE_TYOKI), "引き分けだ！");
+        assert.equal(objConverter(UCHITE_TYOKI, UCHITE_TYOKI), "引き分けだ！");
     });
 
     it('クライアント:パー、サーバ:パー」の場合は、引き分け', function () {
-        assert.equal(objConvetor(UCHITE_PA, UCHITE_PA), "引き分けだ！");
+        assert.equal(objConverter(UCHITE_PA, UCHITE_PA), "引き分けだ！");
     });
 
     it('クライアントをグー固定で1000回対戦してみて、勝ち・負け・引き分け のそれぞれが最低1回以上発生するかというテスト', function () {
@@ -67,7 +67,7 @@ describe('じゃんけんアルゴリズム', function () {
         var resultArray = [];
         for (var i = 0; i < 1001; i++) {
             // クライアントの引数をグーで固定し、janken()を1000回呼び出し
-            resultArray.push(objConvetor(UCHITE_GU));
+            resultArray.push(objConverter(UCHITE_GU));
         }
         // テストで、janken.jsのjdugeResult関数に処理させて返ってきた結果が正しいかどうかを判断するための関数を格納したオブジェクト
         var judgeAdequacy = {
